9a73dcd71df1a7bc3618b555f9b264f7bc67483e
LeTrungHieu-149 <lehieu.149@gmail.com>
2020-08-24 17:07:41 +0700

    fix some small bugs: pause trigger before ended, some UI bugs, update auto hide controls after 3s not move mouse (not completed)

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function (event) {
+window.addEventListener("load", () => {
     const player = document.querySelector(".player");
     const video = document.querySelector(".player__video");
 
@@ -11,13 +11,15 @@ window.addEventListener("load", function (event) {
     const settingsBtn = document.querySelector(".player__settings");
     const fullScrBtn = document.querySelector(".player__full-screen");
 
-    const skipStep=5;
+    const skipStep = 5;
 
     let currentVolume = 0.5;
     let currentTime = 0;
     let duration = 0;
 
     let timeUpdateInterval = null;
+    let loadDuration = null;
+    let timeoutNotMove = null;
 
     const currentTimeSpan = document.querySelector(".current-time");
     const durationTimeSpan = document.querySelector(".duration");
@@ -33,14 +35,25 @@ window.addEventListener("load", function (event) {
         second = second.toString();
 
         if (hour != 0)
-            return `${hour}:${minute.padStart(2, "0")}:${second.padStart(2, "0")}`;
+            return `${hour}:${minute.padStart(2, "0")}:${second.padStart(
+                2,
+                "0"
+            )}`;
         return `${minute.padStart(2, "0")}:${second.padStart(2, "0")}`;
     }
 
-    if (video.readyState === 4) {
-        duration = video.duration;
-        durationTimeSpan.innerHTML = toStringTime(duration);
-        currentTimeSpan.innerHTML = toStringTime(currentTime);
+    loadDuration = setInterval(function () {
+        if (video.readyState > 0) {
+            duration = video.duration;
+            durationTimeSpan.innerHTML = toStringTime(duration);
+            currentTimeSpan.innerHTML = toStringTime(currentTime);
+            clearInterval(loadDuration);
+        }
+    }, 500);
+
+    function toggleControls(event){
+        if(event.type=="mouseleave" && !video.paused) player.classList.remove("is-open");
+        else player.classList.add("is-open");
     }
 
     function handlePlayBtn() {
@@ -99,18 +112,25 @@ window.addEventListener("load", function (event) {
     // when video is ended, change the button
     function updateButton() {
         playBtn.innerHTML = '<i class="fas fa-play"></i>';
-        this.classList.remove("playing");
+        playBtn.classList.remove("playing");
+        currentTime=duration;
+        progressFilled.style.flexBasis = `100%`;
+        currentTimeSpan.innerHTML=toStringTime(currentTime);
     }
 
     video.addEventListener("playing", updateTime);
     video.addEventListener("pause", clearUpdateTime);
+    video.addEventListener("pause",toggleControls);
     video.addEventListener("ended", updateButton);
+
+    player.addEventListener("mouseenter",toggleControls);
+    player.addEventListener("mouseleave",toggleControls);
 
     // when mousedown and video is playing, it will clear old Interval, it's also trigger the playing event, mean it's will trigger the update time
     // and that interval will be deleted when video is paused (pause event) or we mousedown, clear old interval start the new one and so one.
     // This created because if we don't clear old interval, it's still work, and i think, it will decrease our program speed :)
     // when mousedown and video is not playing, it won't trigger the playing event, that's mean update time not work, we need to update the time,
-    // the bar, the currentTime. no need to clearinterval because there is no f*cking interval :)) it's was cleared in the previous pause.
+    // the bar, the currentTime. no need to clear interval because there is no fucking interval :)) it's was cleared in the previous pause.
 
     function changeVideoTime(event) {
         if (timeUpdateInterval) clearInterval(timeUpdateInterval);
@@ -139,26 +159,62 @@ window.addEventListener("load", function (event) {
     }
     fullScrBtn.addEventListener("click", toggleFullScreen);
 
-    function toggleVideoWithoutBtn(event){
-        if(event.target==video) {
+    function toggleVideoWithoutBtn(event) {
+        if (event.target == video) {
             handlePlayBtn();
         }
     }
-    player.addEventListener("click",toggleVideoWithoutBtn);
+    player.addEventListener("click", toggleVideoWithoutBtn);
 
-    function skip(event){
-        if(event.code=="ArrowRight") {
+    function skip(event) {
+        if (event.code == "ArrowRight") {
             clearInterval(timeUpdateInterval);
-            currentTime+=skipStep;
-            video.currentTime=currentTime;
-            currentTimeSpan.innerHTML=toStringTime(currentTime);
+            currentTime += skipStep;
+            video.currentTime = currentTime;
+            currentTimeSpan.innerHTML = toStringTime(currentTime);
         }
-        if(event.code=="ArrowLeft") {
+        if (event.code == "ArrowLeft") {
             clearInterval(timeUpdateInterval);
-            currentTime-=skipStep;
-            video.currentTime=currentTime;
-            currentTimeSpan.innerHTML=toStringTime(currentTime);
+            currentTime -= skipStep;
+            video.currentTime = currentTime;
+            currentTimeSpan.innerHTML = toStringTime(currentTime);
         }
     }
-    window.addEventListener("keydown",skip);
+    window.addEventListener("keydown", skip);
+
+    function handleSettings() {
+        if (!this.classList.contains("is-open")) {
+            this.classList.add("is-open");
+        } else {
+            this.classList.remove("is-open");
+        }
+    }
+    settingsBtn.addEventListener("click", handleSettings);
+
+    // if after 3 seconds u don't move your mouse, controls bar will be hidden
+    function clearTimeoutWhenNotMove(){
+        clearTimeout(timeoutNotMove);
+    }
+
+    video.addEventListener("pause",clearTimeoutWhenNotMove);
+    player.addEventListener("mouseleave",clearTimeoutWhenNotMove);
+
+    function hideControlsWhenNotMove(event){
+        if(video.paused) {
+            console.log("deo dc");
+            return;
+        }
+        console.log("dc ban");
+        player.classList.add("is-open");
+        clearTimeout(timeoutNotMove);
+        timeoutNotMove=setTimeout(()=>{
+            player.classList.remove("is-open");
+            console.log("che ne");
+        },3000);
+    }
+
+    player.addEventListener("mousemove",hideControlsWhenNotMove);
 });
+
+
+
