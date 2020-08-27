0ee8cbb35ec6dfc875d05ec8c1b450b5314ebe26
LeTrungHieu-149 <lehieu.149@gmail.com>
2020-08-27 20:52:49 +0700

    refactor code, adding hover and buffer bar, change updateTime function

--- scripts.js
+++ scripts.js
@@ -4,6 +4,9 @@ window.addEventListener("load", () => {
 
     const progressBar = document.querySelector(".progress");
     const progressFilled = document.querySelector(".progress__filled");
+    const progressHoverBtn = document.querySelector(".progress__hover-button");
+    const progressBuffer = document.querySelector(".progress__buffer");
+    const progressHoverBar = document.querySelector(".progress__hover-bar");
     const soundRange = document.querySelector(".sound__slider");
 
     const playBtn = document.querySelector(".player__play");
@@ -11,39 +14,36 @@ window.addEventListener("load", () => {
     const settingsBtn = document.querySelector(".player__settings");
     const fullScrBtn = document.querySelector(".player__full-screen");
 
+    const currentTimeSpan = document.querySelector(".current-time");
+    const durationTimeSpan = document.querySelector(".duration");
+
     const skipStep = 5;
 
     let currentVolume = 0.5;
     let currentTime = 0;
     let duration = 0;
+    let bufferIndex = 0;
 
-    let timeUpdateInterval = null;
     let loadDuration = null;
     let timeoutNotMove = null;
 
-    let mouseIsHolding=0;
+    let mouseIsHolding = false;
 
-    const currentTimeSpan = document.querySelector(".current-time");
-    const durationTimeSpan = document.querySelector(".duration");
-
+    //parse time in seconds to string
     function toStringTime(timeInSeconds) {
         timeInSeconds = parseInt(timeInSeconds);
         let hour = Math.floor(timeInSeconds / 3600);
         let minute = Math.floor((timeInSeconds - hour * 3600) / 60);
         let second = timeInSeconds - 60 * minute;
-
         hour = hour.toString();
         minute = minute.toString();
         second = second.toString();
-
         if (hour != 0)
-            return `${hour}:${minute.padStart(2, "0")}:${second.padStart(
-                2,
-                "0"
-            )}`;
+            return `${hour}:${minute.padStart(2, "0")}:${second.padStart(2,"0")}`;
         return `${minute.padStart(2, "0")}:${second.padStart(2, "0")}`;
     }
 
+    //load video duration
     loadDuration = setInterval(function () {
         if (video.readyState > 0) {
             duration = video.duration;
@@ -53,11 +53,17 @@ window.addEventListener("load", () => {
         }
     }, 500);
 
-    function toggleControls(event){
-        if(event.type=="mouseleave" && !video.paused) player.classList.remove("is-open");
+    // toggle controls bar
+    function toggleControls(event) {
+        if (event.type == "mouseleave" && !video.paused)
+            player.classList.remove("is-open");
         else player.classList.add("is-open");
     }
+    video.addEventListener("pause", toggleControls);
+    player.addEventListener("mouseenter", toggleControls);
+    player.addEventListener("mouseleave", toggleControls);
 
+    // toggle player button
     function handlePlayBtn() {
         if (!playBtn.classList.contains("playing")) {
             playBtn.innerHTML = '<i class="fas fa-pause"></i>';
@@ -71,6 +77,7 @@ window.addEventListener("load", () => {
     }
     playBtn.addEventListener("click", handlePlayBtn);
 
+    // handle sound range
     function handleSound() {
         video.volume = this.value / 100;
         if (this.value == 0)
@@ -80,6 +87,7 @@ window.addEventListener("load", () => {
     }
     soundRange.addEventListener("input", handleSound);
 
+    // toggle sound button
     function toggleSound() {
         if (!this.parentElement.classList.contains("muted")) {
             video.volume = 0;
@@ -93,90 +101,92 @@ window.addEventListener("load", () => {
     }
     soundBtn.addEventListener("click", toggleSound);
 
-    // update time and bar
-    function updateTime() {
-        timeUpdateInterval = setInterval(() => {
-            currentTime = video.currentTime;
-            currentTimeSpan.innerHTML = toStringTime(currentTime);
-            let currentLengthOfBar = (currentTime / duration) * 100;
-            progressFilled.style.flexBasis = `${currentLengthOfBar}%`;
-        }, 1000);
+    // find index of current buffer in time ranges object
+    function findIndexOfBuffer() {
+        let bufferedLength = video.buffered.length;
+        for (let i = 0; i < bufferedLength; i++) {
+            if (video.buffered.end(i) > currentTime) return i;
+        }
     }
 
-    /* clear old interval */
-    function clearUpdateTime() {
-        clearInterval(timeUpdateInterval);
+    //when time update, change bar's length, time
+    function updateTime() {
+        currentTime = video.currentTime;
+        currentTimeSpan.innerHTML = toStringTime(currentTime);
+        let currentLengthOfProgressBar = currentTime / duration;
+        progressFilled.style.transform = `scaleX(${currentLengthOfProgressBar})`;
+        progressHoverBtn.style.left = `${currentLengthOfProgressBar * 100}%`;
     }
+    video.addEventListener("timeupdate", updateTime);
 
     // when video is ended, change the button
     function updateButton() {
         playBtn.innerHTML = '<i class="fas fa-play"></i>';
         playBtn.classList.remove("playing");
-        currentTime=duration;
-        progressFilled.style.flexBasis = `100%`;
-        currentTimeSpan.innerHTML=toStringTime(currentTime);
     }
-
-    video.addEventListener("playing", updateTime);
-    video.addEventListener("pause", clearUpdateTime);
-    video.addEventListener("pause",toggleControls);
     video.addEventListener("ended", updateButton);
 
-    player.addEventListener("mouseenter",toggleControls);
-    player.addEventListener("mouseleave",toggleControls);
-
-    // when mousedown and video is playing, it will clear old Interval, it's also trigger the playing event, mean it's will trigger the update time
-    // and that interval will be deleted when video is paused (pause event) or we mousedown, clear old interval start the new one and so one.
-    // This created because if we don't clear old interval, it's still work, and i think, it will decrease our program speed :)
-    // when mousedown and video is not playing, it won't trigger the playing event, that's mean update time not work, we need to update the time,
-    // the bar, the currentTime. no need to clear interval because there is no fucking interval :)) it's was cleared in the previous pause.
-
-    function changeVideoTime(event) {
-        mouseIsHolding++;
-        makeProgressBarBigger();
-        if (timeUpdateInterval) clearInterval(timeUpdateInterval);
-        currentTime = (event.offsetX / this.offsetWidth) * duration;
-        video.currentTime = currentTime;
-        let currentLengthOfBar = (currentTime / duration) * 100;
-        progressFilled.style.flexBasis = `${currentLengthOfBar}%`;
-        currentTimeSpan.innerHTML = toStringTime(currentTime);
-        window.addEventListener("mousemove",changeVideoTimeWhenHolding);
+    //progress event trigger when video is loading to buffer, update buffer bar's length
+    function updateBuffer() {
+        bufferIndex = findIndexOfBuffer();
+        let currentLengthOfBufferBar = video.buffered.end(bufferIndex) / duration;
+        progressBuffer.style.transform = `scaleX(${currentLengthOfBufferBar})`;
     }
+    video.addEventListener("progress", updateBuffer);
+    video.addEventListener("playing", updateBuffer);
 
+    //when mouse down, trigger change video time,add event listener to check dragging 
+    function changeVideoTime(event) {
+        mouseIsHolding = true;
+        let currentX = event.pageX - progressBar.getBoundingClientRect().x;
+        currentTime = (currentX / progressBar.offsetWidth) * duration;
+        video.currentTime = currentTime;
+        window.addEventListener("mousemove", changeVideoTimeWhenHolding);
+        window.addEventListener("mouseup", mouseupHandle);
+    }
+    progressBar.addEventListener("mousedown", changeVideoTime);
+
+    //when mousemove, check if mouse is holding to seek
     function changeVideoTimeWhenHolding(event) {
-        if(mouseIsHolding) {
-            let currentX=event.pageX-progressBar.getBoundingClientRect().x;
-            if(currentX > 700) currentX=700;
-            if(currentX<0) currentX=0;
+        if (mouseIsHolding) {
             makeProgressBarBigger();
-            if (timeUpdateInterval) clearInterval(timeUpdateInterval);
+            let currentX = event.pageX - progressBar.getBoundingClientRect().x;
+            if (currentX > progressBar.offsetWidth) currentX = progressBar.offsetWidth;
+            if (currentX < 0) currentX = 0;
             currentTime = (currentX / progressBar.offsetWidth) * duration;
             video.currentTime = currentTime;
-            let currentLengthOfBar = (currentTime / duration) * 100;
-            progressFilled.style.flexBasis = `${currentLengthOfBar}%`;
-            currentTimeSpan.innerHTML = toStringTime(currentTime);
         }
     }
 
-    function makeProgressBarBigger(){
+    //when mouse up, remove unnecessary listener, update mouse holding status
+    function mouseupHandle() {
+        if (mouseIsHolding) {
+            mouseIsHolding = false;
+            window.removeEventListener("mousemove", changeVideoTimeWhenHolding);
+            window.removeEventListener("mouseup", mouseupHandle);
+        }
+    }
+
+    //when mouseenter in progress bar, increase it's size and vice versa
+    function makeProgressBarBigger() {
         progressBar.classList.add("progress--bigger");
     }
 
-    function resetSizeForProgressBar(){
-        progressBar.classList.remove("progress--bigger")
+    function resetSizeForProgressBarAndHideHoverBar() {
+        progressBar.classList.remove("progress--bigger");
+        progressHoverBar.style.transform = `scaleX(0)`;
     }
-    progressBar.addEventListener("mousedown", changeVideoTime);
-    window.addEventListener("mouseup",()=>{
-        if(mouseIsHolding) {
-            mouseIsHolding=0;
-            resetSizeForProgressBar();
-            console.log("end trong win");
-            window.removeEventListener("mousemove",changeVideoTimeWhenHolding);
-        }
-    })
-    progressBar.addEventListener("mouseover",makeProgressBarBigger);
-    progressBar.addEventListener("mouseout",resetSizeForProgressBar);
-    
+
+    function showHoverBar(event) {
+        let currentX = event.pageX - progressBar.getBoundingClientRect().x;
+        let currentLengthOfHoverBar = currentX / progressBar.offsetWidth;
+        progressHoverBar.style.transform = `scaleX(${currentLengthOfHoverBar})`;
+    }
+    progressBar.addEventListener("mouseleave", resetSizeForProgressBarAndHideHoverBar);
+    progressBar.addEventListener("mouseenter",makeProgressBarBigger);
+    progressBar.addEventListener("mousemove", showHoverBar);
+
+    //toggle full screen - copy and paste from w3school with love :))
     function toggleFullScreen() {
         if (player.requestFullscreen) {
             player.requestFullscreen();
@@ -193,6 +203,7 @@ window.addEventListener("load", () => {
     }
     fullScrBtn.addEventListener("click", toggleFullScreen);
 
+    //click on video player to toggle video
     function toggleVideoWithoutBtn(event) {
         if (event.target == video) {
             handlePlayBtn();
@@ -200,15 +211,14 @@ window.addEventListener("load", () => {
     }
     player.addEventListener("click", toggleVideoWithoutBtn);
 
+    //skip 
     function skip(event) {
         if (event.code == "ArrowRight") {
-            clearInterval(timeUpdateInterval);
             currentTime += skipStep;
             video.currentTime = currentTime;
             currentTimeSpan.innerHTML = toStringTime(currentTime);
         }
         if (event.code == "ArrowLeft") {
-            clearInterval(timeUpdateInterval);
             currentTime -= skipStep;
             video.currentTime = currentTime;
             currentTimeSpan.innerHTML = toStringTime(currentTime);
@@ -216,6 +226,7 @@ window.addEventListener("load", () => {
     }
     window.addEventListener("keydown", skip);
 
+    //handle setting button, just it's appearance :)) 
     function handleSettings() {
         if (!this.classList.contains("is-open")) {
             this.classList.add("is-open");
@@ -226,24 +237,20 @@ window.addEventListener("load", () => {
     settingsBtn.addEventListener("click", handleSettings);
 
     // if after 3 seconds u don't move your mouse, controls bar will be hidden
-    function clearTimeoutWhenNotMove(){
+    function clearTimeoutWhenNotMove() {
         clearTimeout(timeoutNotMove);
     }
 
-    video.addEventListener("pause",clearTimeoutWhenNotMove);
-    player.addEventListener("mouseleave",clearTimeoutWhenNotMove);
-
-    function hideControlsWhenNotMove(event){
-        if(video.paused) return;
+    function hideControlsWhenNotMove() {
+        if (video.paused) return;
         player.classList.add("is-open");
         clearTimeout(timeoutNotMove);
-        timeoutNotMove=setTimeout(()=>{
+        timeoutNotMove = setTimeout(() => {
             player.classList.remove("is-open");
-        },3000);
+        }, 3000);
     }
+    player.addEventListener("mousemove", hideControlsWhenNotMove);
+    player.addEventListener("mouseleave", clearTimeoutWhenNotMove);
+    video.addEventListener("pause", clearTimeoutWhenNotMove);
 
-    player.addEventListener("mousemove",hideControlsWhenNotMove);
 });
-
-
-
